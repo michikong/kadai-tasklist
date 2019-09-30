fa2ca1389ba1fbb9f166c299d5854b4e283240c8
michinobu.sekikawa <abc_0615@icloud.com>
2019-09-30 22:18:32 +0900

    11課題：タスク管理アプリにカラムの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,61 @@
 class TasksController < ApplicationController
+  # 共通化をひとまとめに
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
      @tasks = Task.all
   end
 
   def show
-     @task = Task.find(params[:id])
   end
 
   def new
      @task = Task.new
   end
+  
+  def create
+    @task = Task.new(task_params)
+    # binding.pry
+    if @task.save
+       flash[:success] = '内容が正常に保存されました'
+       redirect_to @task
+    else
+       flash.now[:danger] = '内容が保存されませんでした'
+       render :new
+    end
+  end
 
   def edit
-     @task = Task.find(params[:id])
+  end
+  
+  def update
+    if @task.update(task_params)
+       flash[:success] = '内容は正常に更新されました'
+       redirect_to @task
+    else
+       flash.now[:danger] = '内容は更新されませんでした'
+       render :edit
+    end
+  end
+  
+  def destroy
+    @task.destroy
+    
+    flash[:success] = '内容は正常に削除されました'
+    redirect_to tasks_url
+  end
+
+  private
+  
+  # 共通化
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(
+       :content,
+       :status)
   end
 end
